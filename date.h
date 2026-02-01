94e7957bdc11279e6a10b8b0bfe10274ff4c484d
robertmNe <robert.mihai.negoescu@gmail.com>
2026-02-01 12:50:25 -0500

    Add Date class definition with constructors and methods

---
+++ date.h
@@ -0,0 +1,37 @@
+#ifndef DATE_H
+#define DATE_H
+
+#include <iostream>
+using namespace std;
+
+class Date{
+
+    private:
+
+    // Member variables
+    int month;
+    int day;
+    int year;
+
+    public:
+
+    // default constructor
+    Date();
+    // parameter constructor
+    Date(int m, int d, int y);
+    // copy constructor
+    Date(const Date &other);
+
+    // setters 
+    void setMonth (int m);
+    void setDay (int d);
+    void setYear (int y);
+
+    // getters
+    int getMonth() const;
+    int getDay() const;
+    int getYear() const;
+
+};
+
+#endif
