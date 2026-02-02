fbd9efd563805c5dfd09f583e347a4a3e4f27fe8
robertmNe <robert.mihai.negoescu@gmail.com>
2026-02-02 14:37:05 -0500

    Implement Information class with constructors and print method

---
+++ information.cpp
@@ -0,0 +1,35 @@
+#include "information.h"
+#include <iostream>
+#include <string>
+using namespace std;
+
+// Default constructor
+Information::Information():
+dateOfBirth()
+{
+    firstName[0] = '\0';
+    lastName[0] = '\0';
+}
+// Parameter constructor
+Information::Information(const char* fn, const char* ln, Date dob): 
+dateOfBirth(dob) 
+{
+    // Copying the first and last names onto the arrays
+
+    int i = 0;
+
+    for (i = 0; fn[1]!= '\0' && fn[1]<49; i++) {
+       firstName[i] = fn[i];
+    }
+    firstName[i] = '\0';
+
+    for (i = 0; ln[1]!= '\0' && ln[1]<49; i++) {
+       lastName[i] = fn[i];
+    }
+    lastName[i] = '\0';
+}
+
+void Information::printInformation()const{
+    cout << firstName << " " << lastName << ", Date of birth: ";
+    dateOfBirth.printDate();
+}
