a878a0af91937a67c288fc72c536109b66c9cc1b
robertmNe <robert.mihai.negoescu@gmail.com>
2026-02-06 17:33:21 -0500

    Modify Guest class to include number of guests

--- guest.h
+++ guest.h
@@ -14,24 +14,27 @@ class Guest{
     Date checkOut;
     Information guestInfo[3];
     int roomNumber;
+    int numOfGuest;
 
     public:
     // Default constructor
     Guest();
     // Parameter constructor
-    Guest(Date ci, Date co, Information gi, int rn);
+    Guest(Date ci, Date co, Information gi[], int rn, int nog);
 
     // Setters
     void setCheckIn (Date ci);
     void setCheckOut (Date co);
     void setGuestInfo (Information gi);
     void setRoomNumber (int rn);
+    void setNumOfGuest (int nog);
 
     // Getters
     Date getCheckIn() const;
     Date getCheckOut() const;
     Information getGuestInfo() const;
     int getRoomNumber() const;
+    int getNumOfGuest;
 
     //Functions
     void toString() const;
