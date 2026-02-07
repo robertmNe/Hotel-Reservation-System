a992a6c02b8320b3deffa3c5c86332c973f81e2d
s3dnc <smitpatel13306srt@gmail.com>
2026-02-07 18:24:45 -0500

    Update information.cpp

--- information.cpp
+++ information.cpp
@@ -4,32 +4,74 @@
 using namespace std;
 
 // Default constructor
-Information::Information():
-dateOfBirth()
+Information::Information() :
+    dateOfBirth()
 {
     firstName[0] = '\0';
     lastName[0] = '\0';
 }
 // Parameter constructor
-Information::Information(const char* fn, const char* ln, Date dob)
+Information::Information(const char* fn, const char* ln, Date dob) : dateOfBirth(dob)
 {
-    dateOfBirth = dob;
-
     // Copying the first and last names onto the arrays
     int i = 0;
 
-    for (i = 0; fn[1]!= '\0' && fn[1]<49; i++) {
-       firstName[i] = fn[i];
+    // Copy first name
+    while (fn[i] != '\0' && i < 49) {
+        firstName[i] = fn[i];
+        i++;
     }
     firstName[i] = '\0';
 
-    for (i = 0; ln[1]!= '\0' && ln[1]<49; i++) {
-       lastName[i] = fn[i];
+    // Copy last name
+    i = 0;
+    while (ln[i] != '\0' && i < 49) {
+        lastName[i] = ln[i];
+        i++;
     }
     lastName[i] = '\0';
 }
 
-void Information::printInformation()const{
+// Destructor
+Information::~Information() {}
+
+// Setters
+void Information::setFirstName(const char* fn) {
+    int i = 0;
+    while (fn[i] != '\0' && i < 49) {
+        firstName[i] = fn[i];
+        i++;
+    }
+    firstName[i] = '\0';
+}
+
+void Information::setLastName(const char* ln) {
+    int i = 0;
+    while (ln[i] != '\0' && i < 49) {
+        lastName[i] = ln[i];
+        i++;
+    }
+    lastName[i] = '\0';
+}
+
+void Information::setDateOfBirth(Date dob) {
+    dateOfBirth = dob;
+}
+
+// Getters
+const char* Information::getFirstName() const {
+    return firstName;
+}
+
+const char* Information::getLastName() const {
+    return lastName;
+}
+
+Date Information::getDateOfBirth() const {
+    return dateOfBirth;
+}
+
+void Information::printInformation()const {
     cout << firstName << " " << lastName << ", Date of birth: ";
     dateOfBirth.printDate();
 }
