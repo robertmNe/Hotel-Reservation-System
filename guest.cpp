501467136d08066f9ac67a5a52defc7193242576
s3dnc <smitpatel13306srt@gmail.com>
2026-02-07 18:21:22 -0500

    Update guest.cpp

--- guest.cpp
+++ guest.cpp
@@ -15,11 +15,69 @@ Guest::Guest (Date ci, Date co, Information gi[], int rn, int ng)
     checkIn = ci;
     checkOut = co;
     roomNumber = (rn >= 1 && rn <= 20) ? rn: 0;
+    numOfGuest = (ng >= 0 && ng <= 4) ? ng : 0;
 
-    for (int i = 0; i < numOfGuest; i++) {
+    for (int i = 0; i < numOfGuest && i<4; i++) {
         guestInfo[i] = gi[i];
     }
 } 
+
+// Destructor
+Guest::~Guest() {}
+
+// Setters
+void Guest::setCheckIn(Date ci) {
+    checkIn = ci;
+}
+
+void Guest::setCheckOut(Date co) {
+    checkOut = co;
+}
+
+void Guest::setGuestInfo(Information gi) {
+    if (numOfGuest < 4) {
+        guestInfo[numOfGuest++] = gi;
+    }
+}
+
+void Guest::addGuest(const Information& gi) {
+    if (numOfGuest < 4) {
+        guestInfo[numOfGuest++] = gi;
+    }
+}
+
+void Guest::setRoomNumber(int rn) {
+    if (rn >= 1 && rn <= 20) {
+        roomNumber = rn;
+    }
+}
+
+void Guest::setNumOfGuest(int nog) {
+    if (nog >= 0 && nog <= 4) {
+        numOfGuest = nog;
+    }
+}
+
+// Getters
+Date Guest::getCheckIn() const {
+    return checkIn;
+}
+
+Date Guest::getCheckOut() const {
+    return checkOut;
+}
+
+Information* Guest::getGuestInfo() {
+    return guestInfo;
+}
+
+int Guest::getRoomNumber() const {
+    return roomNumber;
+}
+
+int Guest::getNumOfGuest() const {
+    return numOfGuest;
+}
 
 void Guest::toString()const {
     cout << "Room Number: " << roomNumber << endl;
